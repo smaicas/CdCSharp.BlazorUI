172449ba5e91edae3fcaa6e518998f27a15706c1
smaicas <samuel.maicas.development@gmail.com>
2026-01-05 11:11:59 +0100

    Dropdown + DatePîcker initial

---
+++ src/CdCSharp.BlazorUI/Components/Forms/DateTime/BUIInputDatePickerVariant.cs
@@ -0,0 +1,14 @@
+﻿using CdCSharp.BlazorUI.Core.Abstractions.Components.Variants;
+
+namespace CdCSharp.BlazorUI.Components.Forms;
+
+public class BUIInputDatePickerVariant : Variant
+{
+    public static readonly BUIInputDatePickerVariant Outlined = new("Outlined");
+    public static readonly BUIInputDatePickerVariant Filled = new("Filled");
+    public static readonly BUIInputDatePickerVariant Standard = new("Standard");
+
+    public BUIInputDatePickerVariant(string name) : base(name) { }
+
+    public static BUIInputDatePickerVariant Custom(string name) => new(name);
+}
