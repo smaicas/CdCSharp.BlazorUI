411c864715bf5b924221dae6121d524140e3cf79
smaicas <samuel.maicas.development@gmail.com>
2026-01-09 02:39:05 +0100

    .

---
+++ src/CdCSharp.BlazorUI/Components/Layout/Card/BUICardEnums.cs
@@ -0,0 +1,17 @@
+﻿namespace CdCSharp.BlazorUI.Components.Layout;
+
+public enum CardMediaPosition
+{
+    Top,
+    AfterHeader,
+    BeforeActions,
+    Bottom
+}
+
+public enum CardActionsAlignment
+{
+    Start,
+    Center,
+    End,
+    SpaceBetween
+}
--- src/CdCSharp.BlazorUI/Components/Layout/Card/BUICardVariant.cs
+++ src/CdCSharp.BlazorUI/Components/Layout/Card/BUICardVariant.cs
@@ -4,11 +4,13 @@ namespace CdCSharp.BlazorUI.Components.Layout;
 
 public class BUICardVariant : Variant
 {
-    public static readonly BUICardVariant Default = new("Outlined");
-    public static readonly BUICardVariant Outlined = new("Filled");
-    public static readonly BUICardVariant Flat = new("Standard");
+    public static readonly BUICardVariant Elevated = new("Elevated");
+    public static readonly BUICardVariant Outlined = new("Outlined");
+    public static readonly BUICardVariant Flat = new("Flat");
 
-    public BUICardVariant(string name) : base(name) { }
+    public BUICardVariant(string name) : base(name)
+    {
+    }
 
     public static BUICardVariant Custom(string name) => new(name);
 }
