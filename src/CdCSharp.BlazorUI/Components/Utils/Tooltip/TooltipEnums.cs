617d7a4b0e9ab028dce1c4573103d0446d93f637
smaicas <samuel.maicas.development@gmail.com>
2026-01-10 11:51:47 +0100

    .

---
+++ src/CdCSharp.BlazorUI/Components/Utils/Tooltip/TooltipEnums.cs
@@ -0,0 +1,25 @@
+﻿namespace CdCSharp.BlazorUI.Components;
+
+public enum TooltipPlacement
+{
+    Top,
+    TopStart,
+    TopEnd,
+    Bottom,
+    BottomStart,
+    BottomEnd,
+    Left,
+    LeftStart,
+    LeftEnd,
+    Right,
+    RightStart,
+    RightEnd
+}
+
+public enum TooltipTrigger
+{
+    Hover,
+    Focus,
+    HoverAndFocus,
+    Permanent
+}
