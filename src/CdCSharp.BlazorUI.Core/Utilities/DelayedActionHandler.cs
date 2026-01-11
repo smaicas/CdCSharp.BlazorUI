27e8a18dc6de116b363970e56cf00a83b9028f6e
smaicas <samuel.maicas.development@gmail.com>
2026-01-11 02:26:15 +0100

    .

---
+++ src/CdCSharp.BlazorUI.Core/Utilities/DelayedActionHandler.cs
@@ -0,0 +1,47 @@
+﻿// File: Core/Utilities/DelayedActionHandler.cs
+namespace CdCSharp.BlazorUI.Core.Utilities;
+
+public sealed class DelayedActionHandler : IDisposable
+{
+    private CancellationTokenSource? _cts;
+    private readonly object _lock = new();
+
+    public async Task ExecuteWithDelayAsync(Func<Task> action, TimeSpan delay)
+    {
+        Cancel();
+
+        CancellationTokenSource cts;
+        lock (_lock)
+        {
+            _cts = new CancellationTokenSource();
+            cts = _cts;
+        }
+
+        try
+        {
+            await Task.Delay(delay, cts.Token);
+            if (!cts.Token.IsCancellationRequested)
+            {
+                await action();
+            }
+        }
+        catch (TaskCanceledException)
+        {
+        }
+    }
+
+    public void Cancel()
+    {
+        lock (_lock)
+        {
+            _cts?.Cancel();
+            _cts?.Dispose();
+            _cts = null;
+        }
+    }
+
+    public void Dispose()
+    {
+        Cancel();
+    }
+}
---
+++ src/CdCSharp.BlazorUI.Core/Utilities/TimingUtilities.cs
@@ -0,0 +1,37 @@
+﻿// File: Core/Utilities/TimingUtilities.cs
+namespace CdCSharp.BlazorUI.Core.Utilities;
+
+public static class TimingUtilities
+{
+    public static Action<T> Debounce<T>(Action<T> action, TimeSpan interval)
+    {
+        ArgumentNullException.ThrowIfNull(action);
+
+        int last = 0;
+        return arg =>
+        {
+            int current = Interlocked.Increment(ref last);
+            Task.Delay(interval).ContinueWith(_ =>
+            {
+                if (current == last)
+                    action(arg);
+            });
+        };
+    }
+
+    public static Action Debounce(Action action, TimeSpan interval)
+    {
+        ArgumentNullException.ThrowIfNull(action);
+
+        int last = 0;
+        return () =>
+        {
+            int current = Interlocked.Increment(ref last);
+            Task.Delay(interval).ContinueWith(_ =>
+            {
+                if (current == last)
+                    action();
+            });
+        };
+    }
+}
