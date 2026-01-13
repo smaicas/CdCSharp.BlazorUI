88b9ea08b39a8e82c9c5fe0e5fcafe6b55a86ace
smaicas <samuel.maicas.development@gmail.com>
2026-01-13 14:14:39 +0100

    .

---
+++ src/CdCSharp.BlazorUI/Components/Forms/Common/BUIInputVariant.cs
@@ -0,0 +1,16 @@
+﻿using CdCSharp.BlazorUI.Core.Abstractions.Components.Variants;
+
+namespace CdCSharp.BlazorUI.Components.Forms;
+
+public class BUIInputVariant : Variant
+{
+    public static readonly BUIInputVariant Filled = new("Filled");
+    public static readonly BUIInputVariant Outlined = new("Outlined");
+    public static readonly BUIInputVariant Standard = new("Standard");
+
+    public BUIInputVariant(string name) : base(name)
+    {
+    }
+
+    public static BUIInputVariant Custom(string name) => new(name);
+}
